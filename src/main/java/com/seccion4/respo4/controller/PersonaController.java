eee56b0227a6b9515666b3e5fb683c20020b56af
mauri <maur.gajardo@duocuc.cl>
2025-04-30 14:32:17 -0400

    Controller

--- src/main/java/com/seccion4/respo4/controller/PersonaController.java
+++ src/main/java/com/seccion4/respo4/controller/PersonaController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.seccion4.respo4.model.Persona;
 import com.seccion4.respo4.service.PersonaService;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 
@@ -23,6 +27,11 @@ public class PersonaController {
     @PostMapping
     public Persona postPersona(@RequestBody Persona persona){
         return personaService.guardarPersona(persona);
+    }
+    
+    @GetMapping
+    public List<Persona> getPersonas() {
+        return personaService.listarPersonas();
     }
     
     
